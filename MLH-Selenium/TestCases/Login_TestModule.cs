9aa049eff3c090e86f3224b898ecfe9fb0ffc2f1
SelAdv-G3-Minh-Trinh <mysteryit86@outlook.com>
2016-05-09 17:32:11 +0700

    merge

--- MLH-Selenium/TestCases/Login_TestModule.cs
+++ MLH-Selenium/TestCases/Login_TestModule.cs
@@ -247,6 +247,30 @@ namespace MLH_Selenium.TestCases
 
             //Pos: close
             dashboard.Close();
-        }     
+        }
+
+        [TestMethod]
+        public void DA_LOGIN_TC010()
+        {
+            string repo = "SampleRepository";
+            string user = "";
+            string pass = "";
+
+            Console.WriteLine("DA_LOGIN_TC010 - Verify that the page works correctly for the case when no input entered to Password and Username field");
+
+            //1. Navigate to Dashboard login page
+            LoginPage loginPage = new LoginPage();
+            loginPage.open();
+
+            //2. Do not enter username and password
+            //3. Click on "Login" button
+            //VP. Verify that Dashboard Error message "Please enter username!" appears
+            string actual = loginPage.loginWithBlankUser(repo, user, pass).getAlertMessage();
+            string expected = "Please enter username!";
+            Assert.AreEqual(expected, actual);
+
+            // Pos. close:
+            loginPage.Close();
+        }
     }
 }
